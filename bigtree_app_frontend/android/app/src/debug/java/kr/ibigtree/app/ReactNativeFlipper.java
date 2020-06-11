5d78936d261f79f5e3d3a6c69a428e524aef3332
Daehan Kim <daehankim@ibigtree.kr>
2020-06-11 15:29:51 +0900

    패키지 경로 정리

--- bigtree_app_frontend/android/app/src/debug/java/kr/ibigtree/app/ReactNativeFlipper.java
+++ bigtree_app_frontend/android/app/src/debug/java/kr/ibigtree/app/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.bigtree_app_frontend;
+package kr.ibigtree.app;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
--- bigtree_app_frontend/android/app/src/main/java/kr/ibigtree/app/MainApplication.java
+++ bigtree_app_frontend/android/app/src/main/java/kr/ibigtree/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bigtree_app_frontend;
+package kr.ibigtree.app;
 
 import android.app.Application;
 import android.content.Context;
