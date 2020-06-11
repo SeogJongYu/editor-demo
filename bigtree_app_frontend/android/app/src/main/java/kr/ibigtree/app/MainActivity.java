81d9564c25bf193ce2f2cf14e432d0fe59280a69
Daehan Kim <daehankim@ibigtree.kr>
2020-06-11 15:50:03 +0900

    패키지명 정리

--- bigtree_app_frontend/android/app/src/main/java/kr/ibigtree/app/MainActivity.java
+++ bigtree_app_frontend/android/app/src/main/java/kr/ibigtree/app/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "bigtree_app_frontend";
+    return "bigtree_app";
   }
 }
--- bigtree_app_frontend/ios/bigtree_appTests/bigtree_appTests.m
+++ bigtree_app_frontend/ios/bigtree_appTests/bigtree_appTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface bigtree_app_frontendTests : XCTestCase
+@interface bigtree_appTests : XCTestCase
 
 @end
 
-@implementation bigtree_app_frontendTests
+@implementation bigtree_appTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
