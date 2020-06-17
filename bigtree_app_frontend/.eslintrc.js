b64f8b7f64bdcb1cfadcdfd3cc3d3b3c144c9be3
Daehan Kim <daehankim@ibigtree.kr>
2020-06-17 13:06:55 +0900

    들여쓰기 수정

--- bigtree_app_frontend/.eslintrc.js
+++ bigtree_app_frontend/.eslintrc.js
@@ -2,12 +2,17 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'max-len': ['error', 100, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
+    'max-len': [
+      'error',
+      100,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
   },
 };
