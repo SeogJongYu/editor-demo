c1de79fd529f883c2a225c1cc5f9ed7e646723f1
Daehan Kim <daehankim@ibigtree.kr>
2020-10-13 10:20:39 +0900

    jsx-control-statements 플러그인 제거

--- bigtree_app_frontend/babel.config.js
+++ bigtree_app_frontend/babel.config.js
@@ -5,7 +5,6 @@ const COMMON_PLUGINS = [
   '@babel/plugin-syntax-dynamic-import',
   '@babel/plugin-proposal-nullish-coalescing-operator',
   '@babel/plugin-proposal-optional-chaining',
-  'jsx-control-statements',
 ];
 
 const config = {
