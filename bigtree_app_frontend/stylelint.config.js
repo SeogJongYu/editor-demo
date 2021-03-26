840cee7c05e337990600bb97739b3d7e21800df4
Daehan Kim <daehankim@ibigtree.kr>
2021-03-26 10:05:14 +0900

    stylelint 설정 변경

--- bigtree_app_frontend/stylelint.config.js
+++ bigtree_app_frontend/stylelint.config.js
@@ -3,5 +3,9 @@ module.exports = {
   extends: ['stylelint-config-standard'],
   rules: {
     'property-no-unknown': null,
+    'no-eol-whitespace': null,
+    'rule-empty-line-before': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
 };
