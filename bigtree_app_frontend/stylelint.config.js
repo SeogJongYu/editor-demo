30cf6d22302cba59501bb7a45d03c086bf8b2f30
Daehan Kim <daehankim@ibigtree.kr>
2021-04-15 14:28:29 +0900

    stylelint 설정 변경

--- bigtree_app_frontend/stylelint.config.js
+++ bigtree_app_frontend/stylelint.config.js
@@ -6,6 +6,23 @@ module.exports = {
     'no-eol-whitespace': null,
     'rule-empty-line-before': null,
     'at-rule-no-unknown': null,
+    'value-keyword-case': null,
+    'declaration-colon-newline-after': null,
+    'comment-empty-line-before': null,
     'scss/at-rule-no-unknown': true,
+    /* Tailwind 사용시 아래와 같이 설정
+      'scss/at-rule-no-unknown': [
+        true,
+        {
+          ignoreAtRules: [
+            'tailwind',
+            'apply',
+            'variants',
+            'responsive',
+            'screen',
+          ],
+        },
+      ],
+    */
   },
 };
