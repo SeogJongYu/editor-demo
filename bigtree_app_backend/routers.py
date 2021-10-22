575a8dae22da629db85c64b690c9bc1d1c66fc81
Daehan Kim <daehankim@ibigtree.kr>
2021-10-22 10:43:07 +0900

    flake8-import-order 플러그인 추가

--- bigtree_app_backend/routers.py
+++ bigtree_app_backend/routers.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+
 from . import viewsets
 
 router = DefaultRouter()
--- bigtree_app_backend/viewsets.py
+++ bigtree_app_backend/viewsets.py
@@ -1,5 +1,5 @@
-from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
 
 class SampleAPIViewSet(ViewSet):
