719b9a61dee81b0d9c2b304a8ce3d12ed99d00a3
Daehan Kim <daehankim@ibigtree.kr>
2022-01-07 14:46:06 +0900

    webpack ts-checker-webpack-plugin 추가

--- bigtree_app_frontend/webpack.config.js
+++ bigtree_app_frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
@@ -125,6 +126,7 @@ module.exports = {
     new webpack.DefinePlugin({
       __DEV__: isDevelopment,
     }),
+    new ForkTsCheckerWebpackPlugin(),
     isDevelopment && new ReactRefreshWebpackPlugin(),
   ].filter(Boolean),
   optimization: {
